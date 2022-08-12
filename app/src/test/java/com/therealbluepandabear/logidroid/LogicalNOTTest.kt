1f9e071afea3dfa3641e54d645f1276701a04a79
therealbluepandabear <justaguyoney@gmail.com>
2022-08-13 11:34:49 +1200

    Added `LogicalNOTTest`.

---
+++ app/src/test/java/com/therealbluepandabear/logidroid/LogicalNOTTest.kt
@@ -0,0 +1,20 @@
+package com.therealbluepandabear.logidroid
+
+import com.therealbluepandabear.logidroid.models.LogicalAND
+import com.therealbluepandabear.logidroid.models.LogicalNOT
+import com.therealbluepandabear.logidroid.models.UnaryGate
+import org.junit.Test
+
+class LogicalNOTTest {
+    @Test
+    fun logicalNOT_test_1() {
+        val logicalNOT: UnaryGate = LogicalNOT()
+        assert(!logicalNOT.performOperation(b1 = true))
+    }
+
+    @Test
+    fun logicalNOT_test_2() {
+        val logicalNOT: UnaryGate = LogicalNOT()
+        assert(logicalNOT.performOperation(b1 = false))
+    }
+}
